9975b0f001671b2db80d2f2e745d97ee9b93e443
sam-hez <h.epodoi@alustudent.com>
2026-02-19 12:41:48 -0800

    Add directory existence check for safe deployment

--- setup_project.sh
+++ setup_project.sh
@@ -7,11 +7,11 @@
 echo "Starting Automated Project Bootstrapping..."
 
 #Ask the user for project name
-read -p "Enter project name: " input
+read -p "Enter the project name: " input
 
 #Here we validate to make sure the input variable above is not empty
 if [ -z "$input" ]; then
-    echo "Project name cannot be empty."
+    echo "The Project name cannot be empty."
     exit 1
 fi
 
@@ -20,3 +20,9 @@ PROJECT_DIR="attendance_tracker_${input}"
 ARCHIVE_NAME="${PROJECT_DIR}_archive.tar.gz"
 
 #Define Cleanup Login to handle when a user interrupts
+
+#Prevent Overwriting an Existing Directory
+if [ -d "$PROJECT_DIR" ]; then
+    echo "That directory already exists. Exiting to prevent overwrite."
+    exit 1
+fi
