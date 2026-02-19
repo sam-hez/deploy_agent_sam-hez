10f53f12fde4af69d4b1b67190d1d193724ca770
sam-hez <h.epodoi@alustudent.com>
2026-02-19 12:37:43 -0800

    Add dynamic project naming with input validation

--- setup_project.sh
+++ setup_project.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+#AUTOMATED PROJECT BOOTSTRAPPING - BSE SUMMATIVE
+#Author: Samuel Epodoi
+
 
 echo "Starting Automated Project Bootstrapping..."
 
+#Ask the user for project name
+read -p "Enter project name: " input
+
+#Here we validate to make sure the input variable above is not empty
+if [ -z "$input" ]; then
+    echo "Project name cannot be empty."
+    exit 1
+fi
+
+#Create Project Directory variable
+PROJECT_DIR="attendance_tracker_${input}"
+ARCHIVE_NAME="${PROJECT_DIR}_archive.tar.gz"
+
+#Define Cleanup Login to handle when a user interrupts
